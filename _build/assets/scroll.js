fc05ba04d74d750049ef397ad5a1f6f78893a3e1
Piturnah <Piturnah@users.noreply.github.com>
2023-09-01 01:56:21 +0000

    Update build

---
+++ _build/assets/scroll.js
@@ -0,0 +1,18 @@
+window.addEventListener("hashchange", function() {
+    scroll();
+});
+
+$(document).ready(function() {
+    scroll();
+});
+
+function scroll() {
+    var page_url = window.location.href;
+    var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
+
+    if (page_id == "about") {
+        $("html, body").animate({
+            scrollTop: $("#scroll-"+page_id).offset().top - 465/768 * window.screen.height
+        }, "slow");
+    }
+}
